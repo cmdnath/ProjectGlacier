a96fe06bc13438e9c0b6358b71f5ab1c841a95ad
cmdnath <cmdnath@gmail.com>
2019-05-03 08:28:17 -0400

    Added MFC Page and Test Class

--- ProjectGlacier/src/main/java/com/accuweather/glacier/www/pages/MonthlyForeCastPage.java
+++ ProjectGlacier/src/main/java/com/accuweather/glacier/www/pages/MonthlyForeCastPage.java
@@ -12,31 +12,48 @@ private By byMonthTab = By.cssSelector("div.page-subnav > div > div > div.subnav
 private By byListViewCurrentMonth = By.cssSelector("div.page-column-1 > div.monthly-tools > div.card-button.monthly-nav.centered > span");
 private By byListViewLeftArrow = By.cssSelector("div.page-column-1 > div.monthly-tools > div.card-button.monthly-nav.centered > a:nth-child(1) > svg > path");
 private By byListViewTodaySection = By.cssSelector("div.page-column-1 > div.monthly-component > a.forecast-list-card.monthly-list-card.today");
-private By byListViewRightArrow = By.cssSelector("div.page-column-1 > div.monthly-tools > div.card-button.monthly-nav.centered > a.arrow-wrap.is-next > svg > path");
+private By byGridViewRightArrow = By.cssSelector("div.two-column-page-content > div.page-column-1 > div.monthly-tools > div.card-button.monthly-nav.centered > a.arrow-wrap.is-next > svg");
+private By byGridViewLeftArrow = By.cssSelector("div.two-column-page-content > div.page-column-1 > div.monthly-tools > div.card-button.monthly-nav.centered > a:nth-child(1) > svg");
 private By byListViewGridIcon = By.cssSelector("div.page-column-1 > div.monthly-tools > div.layout-selector > a.grid-button > svg > g > path.p2");
 private By byListViewIcon = By.cssSelector("div.page-column-1 > div.monthly-tools > div.layout-selector > a:nth-child(2) > svg > path");
 private By byListViewEntireMonthDayRows = By.cssSelector("div.two-column-page-content > div.page-column-1 > div.monthly-component");
+private By byGridViewMonthYearText = By.cssSelector("div.two-column-page-content > div.page-column-1 > div.monthly-tools > div.card-button.monthly-nav.centered > span");
+
+
 
 /*
- * Method to click Month tab on the AW Monthly Forecast page
- */
-public void clickOnMethodTab() {
-	WebPageLoaded.isDomInteractive();
-	WebElement methodtab = getDriver().findElement(byMonthTab);
-	methodtab.syncVisible(15);
-	methodtab.hover();
-	methodtab.click();
-	}
-
-/*
- * Method to click On Right Arrow on the AW Monthly Forecast page
+ * Method to click On Right Arrow under Grid View of the AW Monthly Forecast page
  */
 public void clickOnRightArrowLink() {
 	WebPageLoaded.isDomInteractive();
-	WebElement methodtab = getDriver().findElement(byListViewRightArrow);
-	methodtab.syncVisible(15);
-	methodtab.hover();
-	methodtab.click();
+	WebElement rightarrowlink = getDriver().findElement(byGridViewRightArrow);
+	rightarrowlink.syncVisible(15);
+	rightarrowlink.hover();
+	rightarrowlink.click();
 	}
 
+/*
+ * Method to click On Left Arrow on the AW Monthly Forecast page
+ */
+public void clickOnLeftArrowLink() {
+	WebPageLoaded.isDomInteractive();
+	WebElement leftarrowlink = getDriver().findElement(byListViewLeftArrow);
+	leftarrowlink.syncVisible(15);
+	leftarrowlink.hover();
+	leftarrowlink.click();
+	}
+
+/*
+ * Method to get Substring of Month from Month Year Span
+ * under Grid View on AW Monthly Forecast page
+ */
+public String  getGridViewMonthSubString() {
+	 WebPageLoaded.isDomInteractive();
+	 final WebElement gridviewmonthText = getDriver().findElement(byGridViewMonthYearText);
+	 gridviewmonthText.syncVisible();
+    String gridviewmonth = gridviewmonthText.getText();
+    String substringmonthname = gridviewmonth.substring(0, gridviewmonth.length()-4).trim();
+    System.out.println("substringmonthname substringmonthname... " +substringmonthname);
+    return substringmonthname;
+    }
 }
--- ProjectGlacier/src/test/java/com/accuweather/glacier/www/monthlyforecast/Test_MonthlyForecastPage.java
+++ ProjectGlacier/src/test/java/com/accuweather/glacier/www/monthlyforecast/Test_MonthlyForecastPage.java
@@ -4,9 +4,11 @@ import org.testng.Assert;
 import org.testng.annotations.Test;
 
 import com.accuweather.glacier.www.AccuWeatherBaseTest;
+import com.accuweather.glacier.www.pages.ForeCastWeatherPage;
 import com.accuweather.glacier.www.pages.LandingPage;
 import com.accuweather.glacier.www.pages.MonthlyForeCastPage;
 import com.chameleon.selenium.web.WebPageLoaded;
+import com.chameleon.utils.date.SimpleDate;
 
 
 public class Test_MonthlyForecastPage extends AccuWeatherBaseTest {
@@ -14,7 +16,9 @@ public class Test_MonthlyForecastPage extends AccuWeatherBaseTest {
 	String ActualForecastPageTitle;
 	String ExpectedForecastPageTitle = "State College Weather - AccuWeather Forecast for PA 16801";
 	private LandingPage landingpage = new LandingPage();
+	private ForeCastWeatherPage forecastpage = new ForeCastWeatherPage();
 	private MonthlyForeCastPage monthlyforecast = new MonthlyForeCastPage();
+	private SimpleDate simpledate = new SimpleDate();
 	 
 	
 	/*
@@ -29,14 +33,62 @@ public class Test_MonthlyForecastPage extends AccuWeatherBaseTest {
 	 * ExpectedForecastPageTitle); }
 	 */
 	
-	@Test(priority=2) public void TC1_navigateToAWMonhtlyWeatherPage() {
-		testStart("Is AW Monthly Weather page Displayed From AW Weather Forecast Page");
+	/*
+	 * @Test(priority=2) public void TC2_NowTabEnabledForeCasthtlyWeatherPage() {
+	 * testStart("Is Now Tab enabled on AW Weather Forecast Page");
+	 * landingpage.enterZipcodeInSearchField(zipcode);
+	 * landingpage.clickOnZipcodeSearchIcon();
+	 * waitUntilElementIsDisplayedOrClickable();
+	 * waitUntilWindowExistsWithTitle(ExpectedForecastPageTitle);
+	 * WebPageLoaded.isDomInteractive(1000);
+	 * Assert.assertTrue(forecastpage.isNowTabEnabled());
+	 * WebPageLoaded.isDomInteractive(1000); }
+	 */
+	
+	/*
+	 * @Test(priority=3) public void TC2_areAllTabsDisplayed() {
+	 * testStart("Are all Tabs displayed on AW Weather Forecast Page");
+	 * landingpage.enterZipcodeInSearchField(zipcode);
+	 * landingpage.clickOnZipcodeSearchIcon();
+	 * waitUntilElementIsDisplayedOrClickable();
+	 * waitUntilWindowExistsWithTitle(ExpectedForecastPageTitle);
+	 * WebPageLoaded.isDomInteractive(1000);
+	 * Assert.assertTrue(forecastpage.isRadarTabDisplayed());
+	 * Assert.assertTrue(forecastpage.isMinuteCastTabDisplayed());
+	 * Assert.assertTrue(forecastpage.isHourlyTabDisplayed());
+	 * Assert.assertTrue(forecastpage.isDailyTabDisplayed());
+	 * Assert.assertTrue(forecastpage.isMonthlyTabDisplayed());
+	 * WebPageLoaded.isDomInteractive(1000); }
+	 */
+	
+	/*
+	 * @Test(priority=4) public void TC2_navigateToAWMonhtlyWeatherPage() {
+	 * testStart("Is AW Monthly Weather page Displayed From AW Weather Forecast Page"
+	 * ); landingpage.enterZipcodeInSearchField(zipcode);
+	 * landingpage.clickOnZipcodeSearchIcon();
+	 * waitUntilElementIsDisplayedOrClickable();
+	 * waitUntilWindowExistsWithTitle(ExpectedForecastPageTitle);
+	 * WebPageLoaded.isDomInteractive(1000); forecastpage.clickOnMonthlyTab();
+	 * WebPageLoaded.isDomInteractive(1000); }
+	 */
+	
+	@Test(priority=5) public void TC5_NavigateToFutureMonthForeCast() {
+		testStart("Navigate to future month forecast from current month forecast");
 		landingpage.enterZipcodeInSearchField(zipcode);
 		landingpage.clickOnZipcodeSearchIcon();
 		waitUntilElementIsDisplayedOrClickable();
 		waitUntilWindowExistsWithTitle(ExpectedForecastPageTitle);
-		WebPageLoaded.isDomInteractive(1000);
-		monthlyforecast.clickOnMethodTab();
-		WebPageLoaded.isDomInteractive(1000);
-		}
+		//WebPageLoaded.isDomInteractive(1000);
+		forecastpage.clickOnMonthlyTab();
+		//WebPageLoaded.isDomInteractive(1000);
+		monthlyforecast.clickOnRightArrowLink();
+		//Assert.assertTrue(forecastpage.isNowTabEnabled());
+		//WebPageLoaded.isDomInteractive(1000);
+		String month = monthlyforecast.getGridViewMonthSubString();
+		System.out.println("month month month month..... " +month);
+		//int months = simpledate.CURRENT_MONTH;
+		//String months = simpledate.getMonthString();
+		//int months = simpledate.getMonth();
+		//System.out.println("months months months... " +months);
+	    }
 }
